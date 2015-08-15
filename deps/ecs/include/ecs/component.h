d9039f6fea5c5b468a6f865aac02f8469f23dc53
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-15 21:02:39 +0100

    Add start of an entity component system framework

---
+++ deps/ecs/include/ecs/component.h
@@ -0,0 +1,12 @@
+#ifndef _COMPONENT_H_
+#define _COMPONENT_H_
+
+#include "platform.h"
+
+class EXPORT Component {
+public:
+    static const unsigned int INVALID_IDENTIFIER = 0;
+    static const unsigned int IDENTIFIER = INVALID_IDENTIFIER;
+};
+
+#endif
---
+++ deps/ecs/include/ecs/component_set.h
@@ -0,0 +1,32 @@
+#ifndef _COMPONENT_SET_H_
+#define _COMPONENT_SET_H_
+
+#include <vector>
+
+#include "platform.h"
+
+class Component;
+
+struct EXPORT ComponentData {
+    Component *component;
+    bool delete_component;
+};
+
+class EXPORT ComponentSet {
+public:
+    ~ComponentSet();
+
+    template<class ComponentClass>
+    bool add_component(ComponentClass *component, bool delete_component = false);
+
+    template<class ComponentClass>
+    ComponentClass *get_component();
+
+    template<class ComponentClass>
+    bool remove_component();
+
+private:
+    std::vector<ComponentData> components;
+};
+
+#endif
