a2dc974e5897d3b955bf65db2953c259d455bdf2
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-15 04:34:42 +0100

    Use spaces instead of tabs

--- deps/logger/include/logger/platform.h
+++ deps/logger/include/logger/platform.h
@@ -2,18 +2,19 @@
 #define _PLATFORM_H_
 
 #ifdef _WIN32
-	#ifdef logger_EXPORTS
-		#define EXPORT __declspec(dllexport)
-	#else
-		#define EXPORT __declspec(dllimport)
-	#endif
+    #ifdef logger_EXPORTS
+        #define EXPORT __declspec(dllexport)
+    #else
+        #define EXPORT __declspec(dllimport)
+    #endif
 
-	#define VSNPRINTF_SAFE(buffer, length, count, format, args) vsnprintf_s(buffer, length, count, format, args)
-	#define VSPRINTF_SAFE(buffer, length, format, args) vsprintf_s(buffer, length, format, args)
+    #define VSNPRINTF_SAFE(buffer, length, count, format, args) vsnprintf_s(buffer, length, count, format, args)
+    #define VSPRINTF_SAFE(buffer, length, format, args) vsprintf_s(buffer, length, format, args)
 #else
-	#define EXPORT
-	
-	#define VSNPRINTF_SAFE(buffer, length, count, format, args) vsnprintf(buffer, length, format, args)
-	#define VSPRINTF_SAFE(buffer, length, format, args) vsprintf(buffer, format, args)
+    #define EXPORT
+
+    #define VSNPRINTF_SAFE(buffer, length, count, format, args) vsnprintf(buffer, length, format, args)
+    #define VSPRINTF_SAFE(buffer, length, format, args) vsprintf(buffer, format, args)
 #endif
+
 #endif
