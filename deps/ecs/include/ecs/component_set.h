432e6c8db492c21841d05822056e4603d044ef21
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-15 23:34:21 +0100

    Move component set implementation to it's own file

--- deps/ecs/include/ecs/component_set.h
+++ deps/ecs/include/ecs/component_set.h
@@ -14,91 +14,17 @@ struct EXPORT ComponentData {
 
 class EXPORT ComponentSet {
 public:
-    ComponentSet() {
-        this->components = new std::vector<ComponentData>();
-    }
-
-    ~ComponentSet() {
-        std::vector<ComponentData> components = *this->components;
-        std::vector<ComponentData>::iterator iterator;
-
-        for (iterator = components.begin(); iterator < components.end(); iterator++) {
-            ComponentData component_data = *iterator;
-
-            if (component_data.delete_component) {
-                delete component_data.component;
-            }
-        }
-
-        delete this->components;
-    }
+    ComponentSet();
+    ~ComponentSet();
 
     template<class ComponentClass>
-    bool add_component(ComponentClass *component, bool delete_component = false) {
-        if (!std::is_base_of<Component, ComponentClass>()) {
-            return false;
-        }
-
-        if (component->IDENTIFIER == Component::INVALID_IDENTIFIER) {
-            return false;
-        }
-
-        if (this->get_component<ComponentClass>() != nullptr) {
-            return false;
-        }
-
-        struct ComponentData component_data = {
-            .component = component,
-            .delete_component = delete_component
-        };
-
-        this->components->push_back(component_data);
-
-        return true;
-    }
+    bool add_component(ComponentClass *component, bool delete_component = false);
 
     template<class ComponentClass>
-    ComponentClass *get_component() {
-        if (!std::is_base_of<Component, ComponentClass>()) {
-            return nullptr;
-        }
-
-        std::vector<ComponentData> components = *this->components;
-        std::vector<ComponentData>::iterator iterator;
-
-        for (iterator = components.begin(); iterator < components.end(); iterator++) {
-            ComponentData component_data = *iterator;
-            ComponentClass *component = static_cast<ComponentClass *>(component_data.component);
-
-            if (component->IDENTIFIER == ComponentClass::IDENTIFIER) {
-                return component;
-            }
-        }
-
-        return nullptr;
-    }
+    ComponentClass *get_component();
 
     template<class ComponentClass>
-    bool remove_component() {
-        if (!std::is_base_of<Component, ComponentClass>()) {
-            return false;
-        }
-
-        std::vector<ComponentData> components = *this->components;
-        std::vector<ComponentData>::iterator iterator = components.begin();
-
-        for (iterator = components.begin(); iterator < components.end(); iterator++) {
-            ComponentData component_data = *iterator;
-            ComponentClass *component = static_cast<ComponentClass *>(component_data.component);
-
-            if (component->IDENTIFIER == ComponentClass::IDENTIFIER) {
-                components.erase(iterator);
-                return true;
-            }
-        }
-
-        return false;
-    }
+    bool remove_component();
 
 private:
     std::vector<ComponentData> *components;
---
+++ deps/ecs/include/ecs/component_set.t.h
@@ -0,0 +1,98 @@
+#ifndef _COMPONENT_SET_T_H_
+#define _COMPONENT_SET_T_H_
+
+#include "component_set.h"
+
+#include <vector>
+
+#include "component.h"
+
+#include "platform.h"
+
+ComponentSet::ComponentSet() {
+    this->components = new std::vector<ComponentData>();
+}
+
+ComponentSet::~ComponentSet() {
+    std::vector<ComponentData> components = *this->components;
+    std::vector<ComponentData>::iterator iterator;
+
+    for (iterator = components.begin(); iterator < components.end(); iterator++) {
+        ComponentData component_data = *iterator;
+
+        if (component_data.delete_component) {
+            delete component_data.component;
+        }
+    }
+
+    delete this->components;
+}
+
+template<class ComponentClass>
+bool ComponentSet::add_component(ComponentClass *component, bool delete_component) {
+    if (!std::is_base_of<Component, ComponentClass>()) {
+        return false;
+    }
+
+    if (component->IDENTIFIER == Component::INVALID_IDENTIFIER) {
+        return false;
+    }
+
+    if (this->get_component<ComponentClass>() != nullptr) {
+        return false;
+    }
+
+    struct ComponentData component_data = {
+        .component = component,
+        .delete_component = delete_component
+    };
+
+    this->components->push_back(component_data);
+
+    return true;
+}
+
+template<class ComponentClass>
+ComponentClass *ComponentSet::get_component() {
+    if (!std::is_base_of<Component, ComponentClass>()) {
+        return nullptr;
+    }
+
+    std::vector<ComponentData> components = *this->components;
+    std::vector<ComponentData>::iterator iterator;
+
+    for (iterator = components.begin(); iterator < components.end(); iterator++) {
+        ComponentData component_data = *iterator;
+        ComponentClass *component = static_cast<ComponentClass *>(component_data.component);
+
+        if (component->IDENTIFIER == ComponentClass::IDENTIFIER) {
+            return component;
+        }
+    }
+
+    return nullptr;
+}
+
+template<class ComponentClass>
+bool ComponentSet::remove_component() {
+    if (!std::is_base_of<Component, ComponentClass>()) {
+        return false;
+    }
+
+    std::vector<ComponentData> components = *this->components;
+    std::vector<ComponentData>::iterator iterator = components.begin();
+
+    for (iterator = components.begin(); iterator < components.end(); iterator++) {
+        ComponentData component_data = *iterator;
+        ComponentClass *component = static_cast<ComponentClass *>(component_data.component);
+
+        if (component->IDENTIFIER == ComponentClass::IDENTIFIER) {
+            components.erase(iterator);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#endif
