3624f8c591b4036f2f5ce3e2a6cd4b482e9ec6d0
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-20 22:14:33 +0100

    Add basic world header

---
+++ deps/ecs/include/ecs/world.h
@@ -0,0 +1,39 @@
+#ifndef _WORLD_H_
+#define _WORLD_H_
+
+#include "ecs/entity.h"
+
+#include <vector>
+
+class World {
+public:
+    World();
+    World(const World &world);
+
+    bool has_entity(Entity::Identifier identifier);
+    bool has_entity(Entity *entity);
+
+    Entity *add_entity(Entity *entity);
+    Entity *create_entity();
+
+    void remove_entity(Entity::Identifier identifier);
+    void remove_entity(Entity *entity);
+
+    std::vector<Entity *> get_entities();
+    Entity *get_entity(Entity::Identifier identifier);
+
+private:
+    struct EntityData {
+        Entity *entity;
+        Entity::Identifier identifier;
+
+        bool operator==(const EntityData &other) {
+            return identifier == other.identifier;
+        }
+    };
+
+    std::vector<EntityData> entities;
+    bool delete_entities;
+};
+
+#endif
