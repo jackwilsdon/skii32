de42b01eeeb26abc44e2225ebab12ca6cb3a9b93
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-21 16:42:50 +0100

    Do not log progname if it is NULL

--- deps/massert/src/massert.cc
+++ deps/massert/src/massert.cc
@@ -7,7 +7,12 @@ MAsserter::MAsserter() : MAsserter(nullptr) {}
 MAsserter::MAsserter(char *progname) : progname(progname) {}
 
 void MAsserter::assert(const char *assertion, const char *message, const char *file, unsigned int line, const char *function) {
-    fprintf(stderr, "%s: %s:%d: %s:\n", progname, file, line, function);
+    if (progname != NULL) {
+        fprintf(stderr, "%s: %s:%d: %s:\n", progname, file, line, function);
+    } else {
+        fprintf(stderr, "%s:%d: %s:\n", file, line, function);
+    }
+
     fprintf(stderr, "Assertion `%s' failed: %s\n", assertion, message);
     printf("Aborted\n");
     std::exit(EXIT_FAILURE);
